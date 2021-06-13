7547f9497ca3df69da45e69787cad927ff7e77be
hadar.amsalem <73754433+hadaramsi@users.noreply.github.com>
2021-06-13 11:58:39 +0300

    loto

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import random
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def loto(hadar, sharon, yarden, chen, startSegment, endSegment):
+    rnd = random.randint(0, 8)
+    sum1 = hadar[rnd] + sharon[rnd] + yarden[rnd] + chen[rnd]
+    if sum1 not in range(startSegment, endSegment + 1):
+        sum1 = sum1 % (endSegment - startSegment + 1)
+        sum1 += startSegment
+    return sum1
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def driver():
+    hadar = [3, 1, 6, 1, 2, 9, 2, 1, 2]
+    sharon = [2, 0, 6, 5, 1, 4, 0, 1, 0]
+    yarden = [3, 1, 8, 2, 3, 0, 6, 5, 3]
+    chen = [2, 0, 7, 2, 7, 8, 0, 2, 9]
+    print("choice for part 1")
+    print(loto(hadar, sharon, yarden, chen, 1, 9))
+    print("choice for part 2")
+    print(loto(hadar, sharon, yarden, chen, 10, 18))
+    print(loto(hadar, sharon, yarden, chen, 10, 18))
+    print("choice for part 3")
+    print(loto(hadar, sharon, yarden, chen, 19, 30))
+    print(loto(hadar, sharon, yarden, chen, 19, 30))
+    print("choice for part 4")
+    print(loto(hadar, sharon, yarden, chen, 31, 36))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+driver()
