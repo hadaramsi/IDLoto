ea3eb38ecc20e63a244bb1dac8c269c9eee67f33
hadar.amsalem <73754433+hadaramsi@users.noreply.github.com>
2021-06-13 12:07:08 +0300

    finish

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ import random
 
 
 def loto(hadar, sharon, yarden, chen, startSegment, endSegment):
+    """
+    :param hadar: Hadar Amsalem's ID
+    :param sharon: Sharon Vazana's ID
+    :param yarden: Yarden Hovav's ID
+    :param chen: Chen Ben Tolila's ID
+    :param startSegment: start Segment of range
+    :param endSegment:  end Segment of range
+    :return: the random choice of question
+    """
     rnd = random.randint(0, 8)
     sum1 = hadar[rnd] + sharon[rnd] + yarden[rnd] + chen[rnd]
     if sum1 not in range(startSegment, endSegment + 1):
@@ -11,6 +20,10 @@ def loto(hadar, sharon, yarden, chen, startSegment, endSegment):
 
 
 def driver():
+    """
+    the main program
+    :return: print results
+    """
     hadar = [3, 1, 6, 1, 2, 9, 2, 1, 2]
     sharon = [2, 0, 6, 5, 1, 4, 0, 1, 0]
     yarden = [3, 1, 8, 2, 3, 0, 6, 5, 3]
@@ -25,5 +38,6 @@ def driver():
     print(loto(hadar, sharon, yarden, chen, 19, 30))
     print("choice for part 4")
     print(loto(hadar, sharon, yarden, chen, 31, 36))
+
 
 driver()
